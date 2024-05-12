c8e5be911de7cc5aa74fb7db0dd6245fd4086490
Iqbal Tawakal <iqtwkl@gmail.com>
2024-05-12 19:05:51 +0700

    fix(build): fix when build

--- frontend/src/pages/role/index.tsx
+++ frontend/src/pages/role/index.tsx
@@ -1,13 +1,17 @@
+import { useState } from 'react';
 import RootLayout from '../../components/layouts/layout';
+import { ApplicationError } from '../../components/common/error';
 
 export function RoleIndexPage() {
+    const [isError, setIsError] = useState(false);
+    const [error] = useState<ApplicationError>(Object);
     const breadcrumbList = [
         { href: '', name: 'Role' },
     ];
 
     return (
         <>
-            <RootLayout breadcrumbList={breadcrumbList} title='Role'>
+            <RootLayout breadcrumbList={breadcrumbList} title='Role' error={error} isError={isError} setIsError={setIsError}>
                 <h1>Role</h1>
                 <div>
                     <p>list role</p>
