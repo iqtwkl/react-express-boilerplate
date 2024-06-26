d1c7cb5c954f7fe7c82a39a00ab26389735eb0bf
Iqbal Tawakal <iqtwkl@gmail.com>
2024-06-26 13:37:43 +0700

    feat(seed): add initial account

---
+++ app/src/db/seed/factories/account.factory.ts
@@ -0,0 +1,16 @@
+import { Account } from '../../../models/Account';
+import { faker } from '@faker-js/faker';
+import { AccountService } from "../../../services/account"
+
+export const createAccountSeed = async () => {
+    const service = new AccountService();
+
+    const account = new Account();
+    account.username = faker.internet.userName();
+    account.email = faker.internet.exampleEmail();
+    account.password = "somepassword";
+    account.is_admin = 1;
+
+    const createdAccount = await service.create(account);
+    console.log(`account added ${createdAccount.username}`);
+};
