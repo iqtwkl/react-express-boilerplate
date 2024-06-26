63499f5664abee2a9263410ecdf4fbb5bbd9d584
Iqbal Tawakal <iqtwkl@gmail.com>
2024-06-26 14:00:25 +0700

    fix(frontend)

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,8 +2,13 @@
 import { Navigate } from 'react-router-dom';
 import RootLayout from '../components/layouts/layout';
 import { useAuth } from '../hooks/AuthContext';
+import { useState } from 'react';
+import { ApplicationError } from '../components/common/error';
 
 export function IndexPage() {
+  const [isError, setIsError] = useState(false);
+  const [error] = useState<ApplicationError>(Object);
+
   const breadcrumbList = [
     { href: '', name: 'Dashboard' },
   ];
@@ -14,7 +19,7 @@ export function IndexPage() {
   }
 
   return (
-    <RootLayout breadcrumbList={breadcrumbList} title='Dashboard'>
+    <RootLayout breadcrumbList={breadcrumbList} title='Dashboard' error={error} isError={isError} setIsError={setIsError}>
         <h1>Dashboard</h1>
         <div>
             <p>chart</p>
