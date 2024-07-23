ef6d5cf707a00889227e43cc318b8890a30a3f99
Iqbal Tawakal <iqtwkl@gmail.com>
2024-07-23 13:34:00 +0700

    chore(protected): kick out when isLoggedIn=false

--- frontend/src/components/common/ProtectedRoute.tsx
+++ frontend/src/components/common/ProtectedRoute.tsx
@@ -3,13 +3,14 @@ import { useEffect, useState } from 'react';
 import { useAuth } from '../../hooks/Auth.hooks';
 import { ErrorUnauthorizedPage } from '../../pages/403';
 import LoadingComponent from './loading';
+import { Navigate } from 'react-router-dom';
 
 interface ProtectedRouteProps {
     element: React.ReactElement;
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ element }) => {
-    const { loggedUser } = useAuth();
+    const { loggedUser, isLoggedIn, token } = useAuth();
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
@@ -17,6 +18,10 @@ const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ element }) => {
             setIsLoading(false);
         }
     }, [loggedUser]);
+
+    if (!isLoggedIn && !token) {
+        return <Navigate to="/auth/login" />;
+      }
     
     return (
     <>  
